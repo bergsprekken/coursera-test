8f876cdeeda4cbb2e5fbfaa6a3065dbf54077872
Martin Seterstøen <martin.seterstoen@gmail.com>
2016-10-19 23:21:18 +0200

    finished except injection

---
+++ module1-solution/app.js
@@ -0,0 +1,42 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', function ($scope) {
+
+  $scope.dishlist = "";
+  $scope.message = "";
+
+  $scope.checkDishList = function () {
+    var tmpMessage = "";
+    if($scope.dishlist == "")
+    {
+      tmpMessage = "Please enter data first";
+    }
+    else{
+      var totalDishes = calculateDishNumber($scope.dishlist);
+      tmpMessage = createMessageResult(totalDishes);
+    }
+    $scope.message = tmpMessage;
+  }
+
+  function calculateDishNumber(string) {
+    var dishStringArray = string.split(",");
+    return dishStringArray.length;
+  }
+
+  function createMessageResult(number) {
+    if(number <= 3) {
+      return "Enjoy!";
+    }
+    else {
+      return "Too much!";
+    }
+  }
+
+});
+
+
+
+})();
